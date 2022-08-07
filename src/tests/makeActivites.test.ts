6b6e7ce420206e17d610dce84adab88d82271ced
Vinicius Silva <viniciusvinisilva1@outlook.com>
2022-08-07 17:01:32 -0300

    chore: use headless on tests

--- src/tests/makeActivites.test.ts
+++ src/tests/makeActivites.test.ts
@@ -3,7 +3,7 @@ import { Ava } from "..";
 test('makeActivites', async () => {
     const ava = new Ava('user', 'password', [])
     const result = await ava.makeActivites('user', 'password', {
-        headless: false,
+        headless: true,
         useToken: 'token',
         useTokenCobaia: 'token'
     })
--- src/tests/readAula.test.ts
+++ src/tests/readAula.test.ts
@@ -3,7 +3,7 @@ import { Ava } from "..";
 test('readAula', async () => {
     const ava = new Ava('user', 'password', [])
     const result = await ava.readAula({
-        headless: false,
+        headless: true,
         useToken: 'token',
     })
     expect(result).toBeInstanceOf(Array)
