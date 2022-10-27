1d8cea7d9e202525c347cafc17387303a98278dc
Vinicius Silva <viniciusvinisilva1@outlook.com>
2022-10-27 18:36:37 -0300

    fix: module realizeAct not found

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -2,5 +2,4 @@ export * from './avaActivites';
 export * from './getAllVideosFromSubject';
 export * from './goToActivites';
 export * from './readAvaVideo';
-export * from './realizeAct';
 export * from './realizeActivites';
